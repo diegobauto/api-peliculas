592bdd09340ecd13fd41c3c95bb0e04e9b03c7d8
unknown <diegobauto00@gmail.com>
2025-09-24 15:55:35 -0500

    feat: Variables railway

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PORT:", process.env.DB_PORT);
+
 // Crear pool de conexiones
 const pool = new Pool({
   user: process.env.DB_USER,
